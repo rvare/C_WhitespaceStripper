1559609f5f1cade35e3a253f8fe32e190a2bda73
Richard Varela <richardvare13@gmail.com>
2025-08-27 19:35:57 -0700

    Created special constant for the double quotation mark.

--- src/main.c
+++ src/main.c
@@ -3,15 +3,17 @@
 #include <string.h>
 #include <ctype.h>
 
+#define QUOTECHAR '"'
+
 void process_string(char token);
 
 int main(int argc, char *argv[]) {
 	char token;
 	
-	while ((token =  getchar()) != EOF) {
+	while ((token=getchar()) != EOF) {
 		if (isspace(token))
 			continue;
-		else if (token == '"')
+		else if (token == QUOTECHAR)
 			process_string(token);
 		else
 			putchar(token);
@@ -20,9 +22,15 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
+/*
+Purpose: Handles the case when double quotation mark is encountered.
+Precondition: token is a is a double quotation mark.
+Postcondition: A closing double quotation marks has been encountered.
+Invarient: None
+*/
 void process_string(char token) {
 	do
 		putchar(token);
-	while ((token = getchar()) != '"');
+	while ((token = getchar()) != QUOTECHAR);
 	putchar(token); // Print the last quote symbol
 }
