cfc9d79738c32630bf869cf5f9ecf3d89bd39fff
Richard Varela <richardvare13@gmail.com>
2025-09-12 16:08:57 -0700

    Now handles single and double quotes.

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 #include <ctype.h>
 #include <string.h>
 
-#define QUOTECHAR '"'
+#define DOUBLE_QUOTECHAR '\"'
+#define SINGLE_QUOTECHAR '\''
 
-void process_string(FILE *);
+void process_string(char, FILE *);
 
 int main(int argc, char *argv[]) {
 	FILE *file_ptr = NULL;
@@ -26,9 +27,9 @@ int main(int argc, char *argv[]) {
 	while ((token=fgetc(file_ptr)) != EOF) {
 		if (isspace(token))
 			continue;
-		else if (token == QUOTECHAR) {
+		else if (token == SINGLE_QUOTECHAR || token == DOUBLE_QUOTECHAR) {
 			putchar(token);
-			process_string(file_ptr);
+			process_string(token, file_ptr);
 		}
 		else
 			putchar(token);
@@ -43,13 +44,14 @@ Precondition: token is a double quotation mark.
 Postcondition: A closing double quotation marks has been encountered.
 Invarient: None
 */
-void process_string(FILE *file_ptr) {
+void process_string(char terminate_quote, FILE *file_ptr) {
 	int token;
-	do {
+
+	while ((token = fgetc(file_ptr)) != terminate_quote) {
 		if (token == EOF) {
 			return;
 		}
 		putchar(token);
-	} while ((token = fgetc(file_ptr)) != QUOTECHAR);
+	}
 	putchar(token); // Print the last quote symbol
 }
