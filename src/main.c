fec92e08d1075ffd322eb1109a5b098080d07d8a
Richard Varela <richardvare13@gmail.com>
2025-09-13 11:10:06 -0700

    Program should now be able to handle escaped quote characters.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <errno.h>
+#include <stdbool.h>
 
 #define DOUBLE_QUOTECHAR '\"'
 #define SINGLE_QUOTECHAR '\''
@@ -52,10 +53,12 @@ Invarient: None
 */
 void process_string(char terminate_quote, FILE *file_ptr) {
 	int token;
+	bool escape_flag = false;
 
-	while ((token=fgetc(file_ptr)) != terminate_quote) {
+	while ((token=fgetc(file_ptr)) != terminate_quote || escape_flag == true) {
 		if (feof(file_ptr) || token == EOF)
 			return;
+		escape_flag = token == '\\' ? true : false;
 		putchar(token);
 	}
 	putchar(token); // Print the last quote symbol
